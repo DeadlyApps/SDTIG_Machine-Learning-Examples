bb817c9f3dec275f4bc01a712b3aa6ee9fb92641
Chris Lucian <Chris@DeadlyApps.com>
2016-01-27 22:36:30 -0800

    Refined the information for the benefit of the SDTIG attendees

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# SDTIG_Machine-Learning-Examples
-Examples for San Diego Technology Immersion Group (SDTIG) Python Machine Learning track.
+# SDTIG Machine Learning Examples
+Examples for San Diego Technology Immersion Group (SDTIG) Python Machine Learning track. This code was written during the meeting to create an experiment from scratch.
+
+## Environment
+Tested with both Anaconda2 and Anaconda3
+
+## Links
+[SDTIG](http://www.meetup.com/San-Diego-Technology-Immersion-Group-SDTIG/)
+
+[Discussion board with links to tools](http://www.meetup.com/San-Diego-Technology-Immersion-Group-SDTIG/messages/boards/thread/49547121)
+
+[Youtube of the meeting when creating these files](https://www.youtube.com/watch?v=KF0ufWzhs6U)
--- iris_prediction.py
+++ iris_prediction.py
@@ -1,51 +1,62 @@
-from random import shuffle
-
+__author__ = "Chris Lucian"
+# numpy is a data shaping and loading module
 import numpy as np
+# sklearn is a library of machne learning algorithms
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import metrics
 from sklearn.svm import SVC
 
+# Load the data from the CSV
 data = np.genfromtxt('iris.txt', delimiter=',')
+# shuffle Data
 np.random.shuffle(data)
-print("data shape",data.shape)
 
-#load data
-#slice the data into taining and test
-train = np.vstack((data[0::3,:], data[1::3,:]))
-test = data[2::3,:]
+print("data shape", data.shape)
 
+# load data
+# slice the data into taining and test
+# Training set is 66% of the data
+train = np.vstack((data[0::3, :], data[1::3, :]))
+# Test set is 33% of the data
+test = data[2::3, :]
+
+# uncomment to shuffle train and test sets
 # np.random.shuffle(train)
 # np.random.shuffle(test)
 
 print(train, test)
 print("train and test shapes", train.shape, test.shape)
 
-#slice class
+# slice to separate inputs and outputs for train and test sets
 
-train_input = train[:,0:4]
-train_output = train[:,4]
+train_input = train[:, 0:4]
+train_output = train[:, 4]
 
 print("train in and out", train_input.shape, train_output.shape)
 
-
-test_input = test[:,0:4]
-test_output = test[:,4]
+test_input = test[:, 0:4]
+test_output = test[:, 4]
 
 print("test in and out", test_input.shape, test_output.shape)
 
-#train an SVC (Suppot Vector Classifier)
+# train an SVC (Suppot Vector Classifier)
 
-classifier = RandomForestClassifier()
+# create the classifier
+classifier = RandomForestClassifier()  # or SVC()
+# learn the data
 classifier.fit(train_input, train_output)
 print(test_output)
+# predict the output of the test input
 predicted = classifier.predict(test_input)
 
 print(predicted)
 
+# Calculate the ROC curve
 fpr, tpr, thresholds = metrics.roc_curve(test_output, predicted, pos_label=2)
+# Calculate the area under the ROC curve
 auc = metrics.auc(fpr, tpr)
 
 print(auc)
 
-#predict the test set values
-#get our AUC and accuracy
+# predict the test set values
+# get our AUC and accuracy
